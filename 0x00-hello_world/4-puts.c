3ccdfb2a61e427b82a73a8d3a5e67735b344fa71
edin93 <il_matador@hotmail.fr>
2019-09-18 16:15:26 +0000

    complete 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main
+ * MAIN, text
  *
- * Return0
+ * return - 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
