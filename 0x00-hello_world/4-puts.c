65d7a97dc8bc43109090a96169c6da9618957c80
edin93 <il_matador@hotmail.fr>
2019-09-18 16:01:48 +0000

    change 4 again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - displays text
+ * main
  *
- * Return 0
+ * Return0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
