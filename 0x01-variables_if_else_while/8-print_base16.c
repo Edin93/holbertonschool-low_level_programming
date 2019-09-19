c4bfbeedd516788b6b1aa9dbca82aa1842365390
edin93 <il_matador@hotmail.fr>
2019-09-19 15:43:47 +0000

    8 is done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char j;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (j = 97; j < 103; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
