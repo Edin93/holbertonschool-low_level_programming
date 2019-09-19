fead2d362eee06415ce7d91e2cbf51e690c125ec
edin93 <il_matador@hotmail.fr>
2019-09-19 18:42:36 +0000

    before last

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,16 +8,21 @@ int main(void)
 {
 int i;
 int j;
+int k;
 for (i = 0; i < 9; i++)
 {
-for (j = i+1; j < 10; j++)
+for (j = i + 1; j < 10; j++)
+{
+for (k = j+1; k < 10; k++)
 {
 putchar(i + '0');
 putchar(j + '0');
-if (i != 9 || j != 9)
+putchar(k + '0');
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
