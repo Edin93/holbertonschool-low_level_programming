477798ff7836b8edf63c9166243648d05954cc2b
edin93 <il_matador@hotmail.fr>
2019-09-23 23:22:55 +0000

    5 changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-  4   * print_sign - prints the sign of a number.
-  5   * @c: parameter type int
-  6   * Return: 0 if the parameter is not a letter, lowercase or uppercase
-  7 **/
+ * print_sign - prints the sign of a number.
+ * @c: parameter type int
+ * Return: 0 if the parameter is not a letter, lowercase or uppercase
+ **/
 int print_sign(int n)
 {
 	if (n > 0)
@@ -16,7 +16,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
