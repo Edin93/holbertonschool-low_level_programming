15043d2daaf36759cbae313c2e23b822c38adc51
edin93 <il_matador@hotmail.fr>
2019-09-24 09:14:50 +0000

    8 is done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
 /**
  * jack_bauer - print all day minutes.
- * display - display hours or minutes.
- * @n: type integer.
+ * @n: type int
  */
 void display(int n);
 void jack_bauer(void)
@@ -14,16 +13,12 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			display(i);
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
-			display(j);
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			_putchar('\n');
 		}
 	}
-}
-
-void display(int n)
-{
-	_putchar(n / 10 + '0');
-	_putchar(n % 10 + '0');
 }
