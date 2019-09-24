423eff09e989bf6dba8ad0c6a1f81db816fb1a3f
edin93 <il_matador@hotmail.fr>
2019-09-24 15:31:15 +0000

    fibo changes

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,13 @@ int main(void)
 {
 	unsigned long i;
 	int arr[50];
+
 	arr[0] = 1;
 	arr[1] = 2;
 
 	for (i = 2; i < 50; i++)
 	{
-		arr[i] = arr[i-1] + arr[i-2];
+		arr[i] = arr[i - 1] + arr[i - 2];
 	}
 	for (i = 0; i < 50; i++)
 	{
