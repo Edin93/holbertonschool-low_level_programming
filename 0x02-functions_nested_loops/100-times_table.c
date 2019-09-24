ddf55e0ad6d2d48c9ae1e103a2046cebaf026d95
edin93 <il_matador@hotmail.fr>
2019-09-24 12:26:54 +0000

    12 is done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,9 +5,7 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int m;
+	int i, j, m;
 
 	if (n >= 0 && n < 16)
 	{
@@ -17,35 +15,30 @@ void print_times_table(int n)
 			{
 				m = i * j;
 				if (m == 0 && j == 0)
-				{
 					_putchar(m + '0');
+				if (m < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(m + '0');
+				}
+				else if (m >= 10 && m < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(m / 10 + '0');
+					_putchar(m % 10 + '0');
 				}
 				else
 				{
-					if (m < 10)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(m + '0');
-					}
-					else if (m >= 10 && m < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(m / 10 + '0');
-						_putchar(m % 10 + '0');
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(m / 100 + '0');
-						_putchar((m / 10) % 10 + '0');
-						_putchar(m % 10 + '0');
-					}
+					_putchar(',');
+					_putchar(' ');
+					_putchar(m / 100 + '0');
+					_putchar((m / 10) % 10 + '0');
+					_putchar(m % 10 + '0');
 				}
 			}
 			_putchar('\n');
