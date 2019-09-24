f62fb84783b199acc54e377907e05d0c95eaba15
edin93 <il_matador@hotmail.fr>
2019-09-24 10:45:55 +0000

    10 is done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - returns the sum of two numbers.
+ * @a: number one
+ * @b: number two
+ * Return: sum of two numbers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
