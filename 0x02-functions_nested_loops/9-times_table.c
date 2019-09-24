528a5fc61318dcbc69b96c83d2ff2337e4ab0dc7
edin93 <il_matador@hotmail.fr>
2019-09-24 10:13:08 +0000

    9 is done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * times_table - print 9 times table.
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int m;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			m = i * j;
+			if (m < 10)
+			{
+				_putchar(m + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
