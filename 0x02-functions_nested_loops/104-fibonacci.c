12541b0960dcb6fec4df0ae1e4c5392efc3b561f
edin93 <il_matador@hotmail.fr>
2019-09-25 11:48:57 +0000

    change last in fibo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,23 +5,33 @@
  */
 int main(void)
 {
-	int i;
-	long double arr[50];
+	int i = 0;
+	unsigned long U0 = 1;
+	unsigned long U1 = 2;
+	unsigned long Un = 0;
 
-	long u0 = 1;
-	long u1 = 2;
-
-	for (i = 2; i < 98; i++)
+	while (i < 98)
 	{
-		arr[i] = arr[i - 1] + arr[i - 2];
-	}
-	for (i = 0; i < 98; i++)
-	{
-		printf("%.0Lf", arr[i]);
+		if (i == 0)
+		{
+			printf("%lu", U0);
+		}
+		else if (i == 1)
+		{
+			printf("%lu", U1);
+		}
+		else
+		{
+			Un = U0 + U1;
+			U0 = U1;
+			U1 = Un;
+			printf("%lu", Un);
+		}
 		if (i < 97)
 		{
 			printf(", ");
 		}
+		i++;
 	}
 	printf("\n");
 	return (0);
