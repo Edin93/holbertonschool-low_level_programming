da6767b5c848230757a79b87ce07de999facc00d
edin93 <il_matador@hotmail.fr>
2019-09-25 18:00:04 +0000

    last one

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,48 +6,30 @@
 int main(void)
 {
 	int i = 0;
-	unsigned long U0 = 1;
-	unsigned long U1 = 2;
-	unsigned long Un = 0;
-	unsigned long p1 = 0;
-	unsigned long p2 = 0;
-	unsigned long l = 4294967295;
-	unsigned long l2 = 1000000000;
-	unsigned long Ur0 = 0;
-	unsigned long Ur1 = 0;
-	unsigned long Ud0 = 0;
-	unsigned long Ud1 = 0;
+	long U0 = 1;
+	long U1 = 2;
+	long Un = 0;
+	long l = 4294967295;
+	long l2 = 1000000000;
 
+	printf("%lu, %lu, ", U0, U1);
 	while (i < 98)
 	{
-		if (i == 0)
-		{
-			printf("%lu", U0);
-		}
-		else if (i == 1)
-		{
-			printf("%lu", U1);
-		}
-		else
-		{
-			Un = U0 + U1;
-			U0 = U1;
-			U1 = Un;
+		Un = U0 + U1;
+		U0 = U1;
+		U1 = Un;
 
-			if (Un > l)
-			{
-				printf("%lu", Un / l2);
-				printf("%lu", Un % l2);
-			}
+		if (Un > l)
+		{
+			printf("%lu", Un / l2);
+			printf("%lu", Un % l2);
+		}
 		else
 		{
 			printf("%lu", Un);
 		}
-	}
 		if (i < 97)
-		{
 			printf(", ");
-		}
 		i++;
 	}
 	printf("\n");
