4c1f3b4a1f8c1ef144d15854c013ff42847054ee
edin93 <il_matador@hotmail.fr>
2019-09-29 13:23:41 +0000

    last must be working now

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,34 +5,33 @@
  */
 void print_number(int n)
 {
-	signed long int limit;
-	signed long int a;
-	signed long int dividor = 1;
+	int limit;
+	int a;
+	int dividor = 1;
 
 	a = n % 10;
-	if (n < 0)
+	n = n / 10;
+	if (n < 0 || a < 0)
 	{
 		n = -n;
 		a = -a;
 		_putchar('-');
 	}
-	if (n < 10)
+	while (n / dividor >= 10)
+		dividor = dividor * 10;
+	limit = dividor;
+	if (n < 10 && n != 0)
 	{
 		_putchar(n + '0');
 	}
-	else
+	else if (n != 0)
 	{
-		n = n / 10;
-		while (n / dividor >= 10)
-			dividor = dividor * 10;
-		limit = dividor;
-
 		while (limit >= 1)
 		{
 			_putchar(n / limit + '0');
 			n = n  % limit;
 			limit = limit / 10;
 		}
-		_putchar(a + '0');
 	}
+	_putchar(a + '0');
 }
