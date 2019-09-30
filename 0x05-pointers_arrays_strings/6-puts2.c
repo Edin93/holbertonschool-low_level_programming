ad48388a1aac963b91bdd7362584f00414e3ea53
edin93 <il_matador@hotmail.fr>
2019-09-30 23:30:07 +0000

    6 is done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character of a string.
+ * @str: string pointer
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int len;
+
+	while ((*(str + i) != '\0'))
+	{
+		i++;
+	}
+	len = i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
