f35dd17cda4fe5376e23679fdf3164b583fe5524
edin93 <il_matador@hotmail.fr>
2019-10-01 14:25:55 +0000

    10 is done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,45 +6,30 @@
  */
 int _atoi(char *s)
 {
-	int i;
-	int num_count = 0;
+	int i = 0;
 	int len = 0;
+	int num = 0;
 	int multi = 1;
-	unsigned long num = 0;
-	int current;
-	int end = 0;
-	int reached_num = 0;
+	int getout = 0;
 
-	while ((*(s + len) != '\0'))
+	while(s[len] != '\0')
 	{
-		if (s[len] == 45 && reached_num != 1)
-		{
-			multi = multi * -1;
-			if (s[len + 1] >= 48 && s[len + 1] <= 57)
-				reached_num = 1;
-		}
-		if (s[len] >= 48 && s[len] <= 57)
-			num_count++;
 		len++;
 	}
 
-	if (num_count == 0)
+	while(i < len && getout == 0)
 	{
-		return (0);
-	}
-	else
-	{
-		i = 0;
-		for (i = 0; i <= len; i++)
+		if (s[i] == 45)
+			multi *= -1;
+		if (s[i] >= 48 && s[i] <= 57)
 		{
-			if (s[i] >= 48 && s[i] <= 57 && end != 1)
-			{
-				current = s[i] - 48;
-				num += num * 10 + (current);
-			}
-			while (s[i + 1] < 48 && s[i + 1] > 57)
-				end = 1;
+			num = num * 10 + (s[i] - 48);
+			if (s[i + 1] >= 48 && s[i + 1] <= 57)
+				getout = 0;
+			else
+				getout = 1;
 		}
+		i++;
 	}
-	return (num * multi);
+	return (multi * num);
 }
