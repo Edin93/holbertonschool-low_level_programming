e9be4d87ff60ed7ed21e28fe7de6fa35dc9eba79
edin93 <il_matador@hotmail.fr>
2019-10-03 16:54:01 +0000

    8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,6 +9,7 @@ char *rot13(char *s)
 	int i, j;
 	char org[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
