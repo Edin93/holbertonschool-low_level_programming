a086188484d72c3e14db1b93a2a543c032ee9082
edin93 <il_matador@hotmail.fr>
2019-10-03 17:30:52 +0000

    100 is done

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * print_number - print numbers
+ * @n: integer
+ */
+void print_number(int n)
+{
+	int limit;
+	int a;
+	int dividor = 1;
+
+	a = n % 10;
+	n = n / 10;
+	if (n < 0 || a < 0)
+	{
+		n = -n;
+		a = -a;
+		_putchar('-');
+	}
+	while (n / dividor >= 10)
+		dividor = dividor * 10;
+	limit = dividor;
+	if (n < 10 && n != 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n != 0)
+	{
+		while (limit >= 1)
+		{
+			_putchar(n / limit + '0');
+			n = n  % limit;
+			limit = limit / 10;
+		}
+	}
+	_putchar(a + '0');
+}
