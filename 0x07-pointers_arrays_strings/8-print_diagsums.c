44db3c6137b9a40e25bd35b76074919f42027259
edin93 <il_matador@hotmail.fr>
2019-10-07 21:27:27 +0000

    7 is done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,14 +11,15 @@ void print_diagsums(int *a, int size)
 	int i;
 	int x1 = 0;
 	int x2 = 0;
+	int ds = size * size;
 
-	for (i = 0; i < size * size; i += size + 1)
+	for (i = 0; i < ds; i += size + 1)
 	{
 		x1 += a[i];
 	}
-	for (i = size - 1; i < (size * size) - 1; i += (size - 1))
+	for (i = size - 1; i < ds - 1; i += (size - 1))
 	{
 		x2 += a[i];
 	}
-	printf("%u, %u\n", x1, x2);
+	printf("%d, %d\n", x1, x2);
 }
