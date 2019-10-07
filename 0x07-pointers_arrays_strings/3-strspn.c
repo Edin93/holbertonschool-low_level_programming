f080d0faf102b7fa5d1f9ceb0feccb97b4957235
edin93 <il_matador@hotmail.fr>
2019-10-07 13:26:26 +0000

    3 is done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: the string.
+ * @accept: the bytes to accept.
+ * Return: the number of bytes in the initial segment of s which consist only
+ * of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int res = 0;
+
+	for (i = 0; s[i] != ' '; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				res++;
+			}
+		}
+	}
+	return (res);
+}
