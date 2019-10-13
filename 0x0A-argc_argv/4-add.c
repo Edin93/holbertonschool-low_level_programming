888fe8b0c2aaa4473c5762a786080f2b52ff4f7d
edin93 <il_matador@hotmail.fr>
2019-10-13 19:56:31 +0000

    4 is done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds 2 positive numbers.
+ * @argc: arguments count.
+ * @argv: arguments vector.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*(argv[i])))
+		{
+		        printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
