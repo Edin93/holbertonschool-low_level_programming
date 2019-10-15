727c21e8c6bc993e568415262ce0cca587090a00
edin93 <il_matador@hotmail.fr>
2019-10-15 13:49:07 +0000

    fibo changed but undone

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void pn(long Un, long a, long b, long ib, long l);
 /**
  * main - prints first 98 fibonacci numbers.
  * Return: 0
@@ -12,7 +13,7 @@ int main(void)
 	long l0 = 1000000000000;
 	long l1 = 4294967295;
 	long l2 = 1000000000;
-	long a, b, z, ib;
+	long a, b, ib;
 	int j;
 
 	printf("%lu, %lu, ", U0, U1);
@@ -23,9 +24,7 @@ int main(void)
 		U1 = Un;
 		a = 0;
 		b = 0;
-		z = 0;
 		ib = 0;
-
 		if (Un >= l1)
 		{
 			if (Un >= l0)
@@ -33,50 +32,14 @@ int main(void)
 				a = Un / l0;
 				b = Un % l0;
 				ib = b;
-				if (a + b == Un)
-				{
-					printf("%lu", a);
-					printf("%lu", b);
-				}
-				else
-				{
-					if (ib / (l0 / 10) < 1)
-					{
-						ib *= 10;
-						z += 1;
-					}
-					printf("%lu", a);
-					for (j = 0; j < z; j++)
-					{
-						printf("0");
-					}
-					printf("%lu", b);
-				}
+				pn(Un, a, b, ib, l0);
 			}
 			else if (Un >= l2)
 			{
 				a = Un / l2;
 				b = Un % l2;
 				ib = b;
-				if (a + b == Un)
-				{
-					printf("%lu", a);
-					printf("%lu", b);
-				}
-				else
-				{
-					if (ib / (l2 / 10) < 1)
-					{
-						ib *= 10;
-						z += 1;
-					}
-					printf("%lu", a);
-					for (j = 0; j < z; j++)
-					{
-						printf("0");
-					}
-					printf("%lu", b);
-				}
+				pn(Un, a, b, ib, l2);
 			}
 		}
 		else
@@ -90,4 +53,36 @@ int main(void)
 	}
 	printf("\n");
 	return (0);
+}
+/**
+ * pn - print number.
+ * @Un: current fibonacci number.
+ * @a: Un / l.
+ * @b: Un % l.
+ * @ib: equals b.
+ * @l: number limit.
+ */
+void pn(long Un, long a, long b, long ib, long l)
+{
+	int j,  z = 0;
+
+	if (a + b == Un)
+	{
+		printf("%lu", a);
+		printf("%lu", b);
+	}
+	else
+	{
+		if (ib / (l / 10) < 1)
+		{
+			ib *= 10;
+			z += 1;
+		}
+		printf("%lu", a);
+		for (j = 0; j < z; j++)
+		{
+			printf("0");
+		}
+		printf("%lu", b);
+	}
 }
