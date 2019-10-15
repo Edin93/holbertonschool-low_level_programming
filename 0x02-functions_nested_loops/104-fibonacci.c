c4a6d11517f71de6b32c4ceecc8297e7bc0e6e2b
edin93 <il_matador@hotmail.fr>
2019-10-15 13:30:55 +0000

    fibo 104 undone

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,6 +12,8 @@ int main(void)
 	long l0 = 1000000000000;
 	long l1 = 4294967295;
 	long l2 = 1000000000;
+	long a, b, z, ib;
+	int j;
 
 	printf("%lu, %lu, ", U0, U1);
 	while (i < 98)
@@ -19,21 +21,63 @@ int main(void)
 		Un = U0 + U1;
 		U0 = U1;
 		U1 = Un;
+		a = 0;
+		b = 0;
+		z = 0;
+		ib = 0;
 
-		if (Un > l1)
+		if (Un >= l1)
 		{
-			if (Un > l0)
+			if (Un >= l0)
 			{
-				printf("%lu", Un / l0);
-				printf("%lu", Un / l0);
+				a = Un / l0;
+				b = Un % l0;
+				ib = b;
+				if (a + b == Un)
+				{
+					printf("%lu", a);
+					printf("%lu", b);
+				}
+				else
+				{
+					if (ib / (l0 / 10) < 1)
+					{
+						ib *= 10;
+						z += 1;
+					}
+					printf("%lu", a);
+					for (j = 0; j < z; j++)
+					{
+						printf("0");
+					}
+					printf("%lu", b);
+				}
 			}
-			else
+			else if (Un >= l2)
 			{
-				printf("%lu", Un / l2);
-				printf("%lu", Un % l2);
+				a = Un / l2;
+				b = Un % l2;
+				ib = b;
+				if (a + b == Un)
+				{
+					printf("%lu", a);
+					printf("%lu", b);
+				}
+				else
+				{
+					if (ib / (l2 / 10) < 1)
+					{
+						ib *= 10;
+						z += 1;
+					}
+					printf("%lu", a);
+					for (j = 0; j < z; j++)
+					{
+						printf("0");
+					}
+					printf("%lu", b);
+				}
 			}
-			printf("%lu", Un / l2);
-			printf("%lu", Un % l2);
 		}
 		else
 		{
@@ -41,6 +85,7 @@ int main(void)
 		}
 		if (i < 97)
 			printf(", ");
+		printf("\n");
 		i++;
 	}
 	printf("\n");
