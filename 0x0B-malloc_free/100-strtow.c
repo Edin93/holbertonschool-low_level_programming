67e89d280f572142ceb8be12b93dd60a17022fda
edin93 <il_matador@hotmail.fr>
2019-10-17 16:49:54 +0000

    added readme to new project

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -8,17 +8,28 @@
  */
 char **strtow(char *str)
 {
-	int i, k, j = 0, words = 1, len = 0;
+	int i, k, j = 0, x, words = 0, len = 0;
 	char **p;
 
 	if (str == NULL || str == '\0')
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 32 && str[i + 1] != 32)
-			words++;
+		if (str[0] == 32)
+		{
+			if (str[i - 1] == 32 && str[i] != 32)
+				words++;
+		}
+		else
+		{
+			if (str[i] == 32 && str[i + 1] != 32)
+				words++;
+		}
 	}
+	printf("words are %d\n", words);
 	p = malloc(sizeof(char *) * words);
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < words; i++)
 	{
 		len = 0;
@@ -29,6 +40,12 @@ char **strtow(char *str)
 			else
 			{
 				p[i] = malloc(1 + len * sizeof(char));
+				if (p[i] == NULL)
+				{
+					for (x = i; x >= 0; x--)
+						free(p[x]);
+					free(p);
+				}
 				for (k = 0; k < len; k++)
 					p[i][k] = str[j - len + k];
 				p[i][len] = '\0';
