e4f253307790ff23b07eb426c107933b385f76e8
edin93 <il_matador@hotmail.fr>
2019-10-20 13:35:04 +0000

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,24 +12,32 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *p = ptr;
 	char *np = ptr;
 	unsigned int i;
+	unsigned int limit;
 
-	if (new_size > old_size)
+	if (ptr == NULL)
 	{
-		for (i = 0; i < new_size; i++)
-		{
-			np[i] = p[i];
-		}
-	}
-	else if (new_size == old_size)
-		return (ptr);
-	else if (ptr == NULL)
 		ptr = malloc(new_size);
+		return (ptr);
+	}
 	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	free(ptr);
+	else if (new_size == old_size)
+		return (ptr);
+	else if (new_size > old_size || old_size > new_size)
+	{
+		free(ptr);
+		if (new_size > old_size)
+			limit = new_size;
+		else
+			limit = old_size;
+		for (i = 0; i < limit; i++)
+		{
+			np[i] = p[i];
+		}
+	}
 	if (np == NULL)
 		free(np);
 	return (np);
