fa190917c548c7aafadb8ab9954048ea273bebfd
edin93 <il_matador@hotmail.fr>
2019-10-21 19:13:39 +0000

    3 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@ int _putchar(char c);
  * @owner: dog owner
  * Description: dog struct type.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	double age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
