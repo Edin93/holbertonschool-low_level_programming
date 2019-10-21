e1f01ed55c1c977e7e473a86cc2d6ebffa0c3a2d
edin93 <il_matador@hotmail.fr>
2019-10-21 19:37:23 +0000

    4 done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,6 +11,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggy;
+	int nlen = 0, olen = 0;
+	char *dogName, *dogOwner;
 
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
@@ -18,5 +20,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	doggy->name = name;
 	doggy->age = age;
 	doggy->owner = owner;
+
+	while ((*doggy).owner[nlen] != '\0')
+		nlen++;
+	while ((*doggy).owner[olen] != '\0')
+		olen++;
+
+	dogName = malloc(sizeof(char) * nlen);
+	if (dogName == NULL)
+		free(dogName);
+	dogOwner = malloc(sizeof(char) * olen);
+	if (dogOwner == NULL)
+		free(dogOwner);
 	return (doggy);
 }
