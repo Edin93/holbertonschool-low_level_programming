917d680c9c2de17d82833cdd22f82f8aee557c6c
edin93 <il_matador@hotmail.fr>
2019-10-21 18:14:56 +0000

    0 done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+/**
+ * struct dog - define type struct dog.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: dog struct type.
+ */
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+
+};
+#endif
