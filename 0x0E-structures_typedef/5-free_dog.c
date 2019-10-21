bc5948fc99c0c8154e4466939ec8f6436873d330
edin93 <il_matador@hotmail.fr>
2019-10-21 20:43:17 +0000

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * free_dog - free dogs
@@ -6,8 +7,10 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d.name);
-	free(d.age);
-	free(d.owner);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
