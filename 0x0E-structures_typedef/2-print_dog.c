e1bc6a2ba1b0481e8ac291ad03479cb556719fae
edin93 <il_matador@hotmail.fr>
2019-10-21 19:09:24 +0000

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", (*d).name);
-		if ((*d).age == '\0')
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %.1f\n", (*d).age);
+		printf("Age: (nil)\n");
 		if ((*d).owner == NULL)
 			printf("Owner: (nil)\n");
 		else
