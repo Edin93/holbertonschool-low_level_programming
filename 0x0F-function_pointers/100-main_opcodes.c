310da298b99e5d22898366dde95d2cbad8b5e99d
edin93 <il_matador@hotmail.fr>
2019-10-23 23:15:50 +0000

    advanced done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,3 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * main - prints opcodes of itself.
+ * @argc: arguments count.
+ * @argv: arguments vector.
+ * Return: integer.
+ */
+int main(int argc, char *argv[])
+{
+	int nb = atoi(argv[1]);
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (nb < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	char *p = (char *)&main;
+
+	for (i = 0; i < nb; i++)
+	{
+		printf("%.2hhx", p[i]);
+		if (i != nb - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
