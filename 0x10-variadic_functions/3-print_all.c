818914a87926b93fd42f6e1a7c742991f0d71709
edin93 <il_matador@hotmail.fr>
2019-10-26 19:24:08 +0000

    3 done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -61,7 +61,7 @@ void print_all(const char * const format, ...)
 
 	va_start(things, format);
 
-	while (format[i] != '\0' && format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
 		while (forms[j].name != NULL)
