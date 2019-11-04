21d1ab16cdb1858bad98d82391e60166dca24f32
edin93 <il_matador@hotmail.fr>
2019-11-04 11:31:35 +0000

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list.
+ * @head: list head.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *new = NULL;
+
+	while(head != NULL)
+	{
+		new = head->next;
+		free(head);
+		head = new;
+	}
+}
