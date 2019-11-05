05d1d992e4a38362a1f1091293176ef93b43e469
edin93 <il_matador@hotmail.fr>
2019-11-05 12:10:45 +0000

    task 10 fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (i == index)
 			break;
 		temp = p;
-		if (i != index && p->next != NULL)
+		if (i != index)
 		{
 			p = p->next;
 		}
