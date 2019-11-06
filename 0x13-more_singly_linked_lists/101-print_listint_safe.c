a6864fd779ae2fd903d2967f627130b8d56a52f0
edin93 <il_matador@hotmail.fr>
2019-11-06 14:48:24 +0000

    task 12 done

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,6 +12,9 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *fp = head;
 	const listint_t *p = head;
 
+	if (head == NULL)
+		exit(98);
+
 	while (sp != NULL && fp != NULL && fp->next != NULL)
 	{
 		sp = sp->next;
