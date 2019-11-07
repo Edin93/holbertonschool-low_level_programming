7308dda093d80fa0fe0b6beca7188ad3616a4257
edin93 <il_matador@hotmail.fr>
2019-11-07 20:49:14 +0000

    task 4 done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,8 +9,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int a = 1 << index;
 
-	if (index > 31 || (a > *n))
+	if (index > 31)
 		return (-1);
-	*n -= a;
+	*n &= ~(a);
 	return (1);
 }
