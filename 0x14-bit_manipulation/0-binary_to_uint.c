b9f07f7ab016fc46d43e5b737172ae544a8c8483
edin93 <il_matador@hotmail.fr>
2019-11-07 18:09:15 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: binary representation of the number.
+ * Return: the converted number, 0 if b has character other than 0, 1 or NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int r = 0;
+	unsigned int len = 0, i;
+
+	if (b == NULL)
+		return (0);
+	while (b[len])
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		len++;
+	}
+	len--;
+	for (i = 0; b[i]; i++, len--)
+	{
+		if (b[i] == '1')
+			r += 1 << len;
+	}
+	return (r);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HEAD
+#define HEAD
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+#endif
