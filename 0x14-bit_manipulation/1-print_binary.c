7843f5fa85c476ce605239a61afbf5828eebbcae
edin93 <il_matador@hotmail.fr>
2019-11-07 19:15:24 +0000

    1 done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,28 @@
 #include "holberton.h"
 /**
+ * show - prints the binary representation of a number.
+ * @n: number to convert to binary.
+ */
+void show(unsigned long int n)
+{
+	if (n != 0)
+	{
+		show(n >> 1);
+		if (n & 1)
+			_putchar('1' + 0);
+		if (!(n & 1))
+			_putchar('0' + 0);
+
+	}
+}
+/**
  * print_binary - prints the binary representation of a number.
  * @n: number to convert to binary.
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (n != 0)
+		show(n);
+	else
 		_putchar('0' + 0);
-	while (n != 0)
-	{
-		if (n & 1)
-			_putchar('1' + 0);
-		else
-			_putchar('0' + 0);
-		n = n >> 1;
-	}
 }
