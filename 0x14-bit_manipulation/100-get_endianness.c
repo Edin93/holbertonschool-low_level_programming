3a9cb41f12b90ebd71e624e6a880cfb2e3eb05c7
edin93 <il_matador@hotmail.fr>
2019-11-08 15:34:48 +0000

    endianness it is git add .

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * get_endianness - checks the endianness.
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	int *p;
+	int a = 1;
+
+	p = &a;
+
+	if ((*p >> 24) & 1)
+		return (1); /* little endian */
+	else
+		return (0); /* big endian */
+}
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,5 +16,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		count += x & 1;
 		x = x >> 1;
 	}
+
 	return (count);
 }
