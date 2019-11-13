cef4a9443b7387a46cc5dcf1a2b1d9c371224fc2
edin93 <il_matador@hotmail.fr>
2019-11-13 02:24:09 +0000

    chante task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,18 +42,16 @@ int main(int argc, char *argv[])
 
 	file_from = argv[1];
 	file_to = argv[2];
-	ffd = open(file_from, O_RDONLY);
 
+	ffd = open(file_from, O_RDWR);
 	if (ffd == -1)
 		perr1("Error: Can't read from file ", argv[1], 98);
 
 	ftd = open(file_to, O_CREAT | O_TRUNC | O_APPEND | O_RDWR, 0664);
-
 	if (ftd == -1)
 		perr1("Error: Can't write to ", argv[2], 99);
 
 	content = malloc(sizeof(char) * 1024);
-
 	while ((rff = read(ffd, content, 1024)) != 0)
 	{
 		if (rff == -1)
