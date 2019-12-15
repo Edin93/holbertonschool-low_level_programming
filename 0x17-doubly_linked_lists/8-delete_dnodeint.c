cfccf12ab3387106e23f1bf18c224eee9207c826
edin93 <il_matador@hotmail.fr>
2019-12-15 04:17:33 +0000

    changed 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,12 +7,13 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp = *head;
+	dlistint_t *tmp;
 	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 
+	tmp = *head;
 	if (index == 0 && tmp)
 	{
 		if (tmp->next)
