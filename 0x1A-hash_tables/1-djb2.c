d03792fdd7f572d0c175e7fa84dde480df32d7f0
edin93 <il_matador@hotmail.fr>
2020-01-19 21:23:43 +0100

    task 1 done

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,7 +5,7 @@
 /**
  * hash_djb2 - hash function implementing the djb2 algorithm.
  * @str: string value of the new node to insert.
- * Return: 
+ * Return: hashed value of string as an unsigned long int.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
