2903d3e6337071b453c1c09bbc2efac2f4727f2f
edin93 <il_matador@hotmail.fr>
2020-01-19 20:59:07 +0100

    task 0 memory allocation for array fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	arr = (hash_node_t *)malloc(size * sizeof(hash_node_t));
+	arr = (hash_node_t *)malloc(size * sizeof(hash_node_t *));
 	if (arr == NULL)
 		return (NULL);
 	ht->size = size;
