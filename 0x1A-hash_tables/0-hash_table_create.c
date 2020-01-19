ddce595a767bbed92d8fceb2d7abda490875814b
edin93 <il_matador@hotmail.fr>
2020-01-20 00:49:28 +0100

    fix variable initialization issue.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,18 +10,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
-	hash_node_t *arr = NULL;
+	hash_node_t **arr = NULL;
 
 	if (size <= 0)
 		return (NULL);
 
-	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	arr = (hash_node_t *)malloc(size * sizeof(hash_node_t *));
+	arr = malloc(size * sizeof(hash_node_t *));
 	if (arr == NULL)
 		return (NULL);
 	ht->size = size;
-	ht->array = &arr;
+	ht->array = arr;
 	return (ht);
 }
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -13,7 +13,15 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
+
 	hash_table_set(ht, "betty", "holberton");
-	hash_table_set(ht, "cisfun", "maybe");
+	hash_table_set(ht, "hetairas", "maybe");
+	hash_table_set(ht, "mentioner", "not");
+
+	hash_table_set(ht, "stylist", "not me");
+	hash_table_set(ht, "stylist", "probably you!");
+
+	hash_table_set(ht, "heliotropes", "sick");
+	hash_table_set(ht, "neurospora", "angel");
 	return (EXIT_SUCCESS);
 }
