d98871bf28b3a096af3479af1177f63438774328
edin93 <il_matador@hotmail.fr>
2020-01-20 11:18:15 +0100

    task 6, freeing working, remains errors to fix.

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,4 +7,29 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	hash_node_t *lh, *node, *next;
+
+	if (!ht)
+		return;
+
+	while (i < ht->size)
+	{
+		lh = ht->array[i];
+		if (lh)
+		{
+			node = lh;
+			while (node)
+			{
+				next = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = next;
+			}
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
 }
