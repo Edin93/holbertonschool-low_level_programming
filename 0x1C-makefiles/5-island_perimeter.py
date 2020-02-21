c5d15f282b24199937cedc596bdf60ea463e6583
edin93 <il_matador@hotmail.fr>
2020-02-21 19:47:55 +0100

    task 5 done

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains the island_perimeter function that returns the perimiter of
-the island described in the grid parameter of the function.
+This module contains the island_perimeter function that returns the perimiter
+of the island described in the grid parameter of the function.
 """
 
 
@@ -13,3 +13,20 @@ def island_perimeter(grid):
     Parameters:
     grid (list): list of list of int describing the island to calculate.
     """
+    p = 0
+    g = grid
+    for ri in range(len(g)):
+        for ci in range(len(g[ri])):
+            if g[ri][ci] == 1:
+                p += 4
+                if g[ri][ci - 1] != len(g[ri]) - 1 and g[ri][ci + 1] == 1:
+                    p -= 1
+                if g[ri][ci] != 0 and g[ri][ci - 1] == 1:
+                    p -= 1
+                pr = ri - 1
+                if g[ri] != 0 and g[pr][ci] == 1:
+                    p -= 1
+                nr = ri + 1
+                if ri != len(g) - 1 and g[nr][ci] == 1:
+                    p -= 1
+    return p
