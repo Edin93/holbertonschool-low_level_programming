ed4492e7882d87f7a98e77f636fd4ca2adc4999a
edin93 <il_matador@hotmail.fr>
2020-04-16 04:25:17 +0100

    task 7 done

---
+++ 0x1E-search_algorithms/backup100.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ * jump_search - Searches for a value in a sorted array of integers using
+ * jump search algorithms.
+ * @array: array to search.
+ * @size: size of the array.
+ * @value: value to search for.
+ * Return: index of value, -1 otherwise.
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int step, i, j;
+
+	if (array == NULL)
+		return (-1);
+	step = sqrt(size);
+	for (i = 0; i < (int)size || array[i] < value; i += step)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (i + step < (int)size && array[i + step] >= value)
+		{
+			printf("Value found between indexes [%d] and [%d]\n",
+			       i, i + step);
+			for (j = i; j <= i + step; j++)
+			{
+				printf("Value checked array[%d] = [%d]\n",
+				       j, array[j]);
+				if (array[j] == value)
+					return (j);
+			}
+		}
+	}
+	return (-1);
+}
