cc02de958e295dd3bcfe482ca08c22620e48cf1b
edin93 <il_matador@hotmail.fr>
2020-04-16 23:55:25 +0100

    task 14 and 15 done

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,74 @@
+#include "search_algos.h"
+/**
+ * bin_search - searches for a value in a sorted array of ints
+ * using binary search algorithm.
+ * @array: pointer to to the 1st element of the array to search.
+ * @value: value to search for.
+ * @start: start index.
+ * @end: end index.
+ * Return: Returns the index of the found value, otherwise returns -1.
+ */
+int bin_search(int *array, int value, int start, int end)
+{
+	int min, max, i, mid;
+
+	if (array == NULL)
+		return (-1);
+	min = start;
+	max = end;
+	while (max >= min)
+	{
+		printf("Searching in array: ");
+		for (i = min; i <= max; i++)
+		{
+			printf("%d", array[i]);
+			if (i != max)
+				printf(", ");
+		}
+		printf("\n");
+		mid = (max + min) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] > value)
+			max = mid - 1;
+		else if (array[mid] < value)
+			min = mid + 1;
+	}
+	return (-1);
+}
+/**
+ * exponential_search - Search for a value in a sorted array of integers
+ * using the exponential search algorithm.
+ * @array: array to search.
+ * @size: array size.
+ * @value: value to search.
+ * Return: the first index where value is located, -1 otherwise.
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	int i, sz, min;
+
+	sz = (int)size;
+	if (array == NULL || size == 0)
+		return (-1);
+	if (value == array[0])
+	{
+		printf("Value checked array[%d] = [%d]\n", 0, array[0]);
+		return (0);
+	}
+	for (i = 1; i < sz; i *= 2)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] >= value)
+		{
+			printf("Value found between indexes [%d] and [%d]\n",
+			       i, i * 2);
+			if (i + 1 <= sz)
+				min = i + 1;
+			else
+				min = sz;
+			return (bin_search(array, size, i, min));
+		}
+	}
+	return (-1);
+}
