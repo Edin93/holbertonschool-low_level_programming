41f43a9a32d32622b40866f8a6484c20ef96e7b5
edin93 <il_matador@hotmail.fr>
2020-04-17 07:25:01 +0100

    task 12 init

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,10 +1,44 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
  * jump_list - Searches for a value in a sorted list of integers using
  * the jump search algorithm.
- * @list: 
+ * @list: a pointer to the head of the list to search in.
+ * @size: the number of nodes in a list.
+ * @value: the value to search for.
+ * Return:  a pointer to the first node where value is located, otherwise -1.
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
+	int i, j, step, sz;
+	listint_t *prev, *next;
+
+	if (list == NULL)
+		return (NULL);
+	sz = (int)size;
+	step = sqrt(sz);
+	prev = list;
+	next = prev;
+	for (i = step; i < sz; i += step)
+	{
+		prev = next;
+		for (j = 0; j <= step && next != NULL; j++)
+			next = next->next;
+		printf("Value checked at index [%d] = [%d]", i, next->n);
+		if (next->n >= value || next == NULL)
+		{
+			printf("Value found between indexes [%d] and [%d]",
+			       i - step, i);
+			for (j = i; j <= step + i && next != NULL; j++)
+			{
+				printf("Value checked at index [%d] = [%d]",
+				       i, next->n);
+				next = next->next;
+				if (next->n == value)
+					return (next);
+			}
+		}
+	}
+	return (NULL);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -48,4 +48,7 @@ typedef struct skiplist_s
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
 #endif
